3167bb2fa900e1b5065f24335c187ce5de5725fb
jakem <jaka.meglic@gmail.com>
2024-04-25 14:23:53 +0200

    Build home page

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -15,5 +15,5 @@ class UserSerializer(serializers.ModelSerializer):
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ["id", "tile", "content", "created_at", "author"]
+        fields = ["id", "title", "content", "created_at", "author"]
         extra_kwargs = {"author": {"read_only": True}}

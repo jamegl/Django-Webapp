7de92fff79848ce6cf7bbdd96df236480dfd725c
jakem <jaka.meglic@gmail.com>
2024-04-29 01:28:31 +0200

    Add 'axios' to the list of external dependencies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['jwt-decode', 'react-router-dom'],
+      external: ['jwt-decode', 'react-router-dom', 'axios'],
     },
   },
 });
